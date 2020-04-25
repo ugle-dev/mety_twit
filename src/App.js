d52e003a676dd46f4838341f13e687fa23edd213
Serbay ACAR <serbayacar@gmail.com>
2020-04-26 01:14:36 +0300

    Router :: init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Switch, Route, Router } from 'react-router-dom';
+import history from './helpers/history';
 
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import AppBar  from '@material-ui/core/AppBar';
 import Toolbar  from '@material-ui/core/Toolbar';
 import Link from '@material-ui/core/Link';
+
+import BotsLayout from './layouts/bots_layouts'
+import BotDetailLayout from './layouts/botDetail_layouts'
 
 function Copyright() {
   return (
@@ -29,6 +34,15 @@ function App() {
           Methy Twitter Bots
         </Typography>
       </Toolbar>
+
+
+      <Router history={history}>
+        <Switch>
+          <Route path="/" exact component={BotsLayout} />
+          <Route path="/bot" component={BotDetailLayout} />
+        </Switch>
+      </Router>
+    
     </AppBar>
     </Container>
   );
---
+++ src/layouts/botDetail_layouts.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class BotsDetailLayout extends React.Component{
+    constructor( props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                Hello Bot Detail Layout
+            </div>
+        )
+    }
+}
+
+export default BotsDetailLayout;
