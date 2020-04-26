612e8116786f475d04a49be31919a642473fecda
Serbay ACAR <serbayacar@gmail.com>
2020-04-26 18:02:03 +0300

    Bot Details :: skeleton upgrade

--- src/components/wrappers/bot.js
+++ src/components/wrappers/bot.js
@@ -3,6 +3,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import { TextField , Button} from '@material-ui/core';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,8 +20,39 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <Typography
+        component="div"
+        role="tabpanel"
+        hidden={value !== index}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && <Box p={3}>{children}</Box>}
+      </Typography>
+    );
+}
+
+function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
 export default function BotContainer() {
   const classes = useStyles();
+
+  const states = {
+      value: 0
+  }
+
+  const handleChange = (event, newValue) => {
+    states.value = newValue;
+  };
 
   return (
     <div className={classes.root}>
@@ -50,17 +86,19 @@ export default function BotContainer() {
             
           </Paper>
         </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        <Grid item xs={12} >
+            <AppBar position="static">
+                <Tabs aria-label="simple tabs example" onChange={handleChange} value={states.value} >
+                    <Tab label="Twits"   {...a11yProps(0)}/>
+                    <Tab label="Logs"  {...a11yProps(1)}/>
+                </Tabs>
+            </AppBar>
+            <TabPanel value={states.value}  index={0}>
+                Twits
+            </TabPanel>
+            <TabPanel value={states.value} index={1}>
+                Logs
+            </TabPanel>
         </Grid>
       </Grid>
     </div>
