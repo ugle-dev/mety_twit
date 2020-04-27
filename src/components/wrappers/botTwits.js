2be11e3424808482dae541dc5f2dd330fba4e61a
Serbay ACAR <serbayacar@gmail.com>
2020-04-27 22:24:01 +0300

    Rearrange functions

--- src/components/wrappers/botTwits.js
+++ src/components/wrappers/botTwits.js
@@ -6,7 +6,38 @@ import Tab from '@material-ui/core/Tab';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 
-function TabPanel(props) {
+//Table Imports
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+
+const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
+
+
+function createData(name, calories) {
+    return { name, calories};
+}
+
+
+function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+
+  function TabPanel(props) {
     const { children, value, index, ...other } = props;
   
     return (
@@ -22,15 +53,44 @@ function TabPanel(props) {
     );
 }
 
-function a11yProps(index) {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
+
+function LogTable( props ) {
+    const classes = useStyles();
+    const { data } = props;
+
+    return (
+        <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Twits</TableCell>
+              <TableCell align="right">Detail</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
 }
+
 
 export default function BotTwitsContainer( props) {
   const { handleTabChange, tabValue} = props;
+
+
+  const rows = [
+    createData('Gingerbread', 356),
+  ];
+
 
   return (
     <>
@@ -45,7 +105,7 @@ export default function BotTwitsContainer( props) {
                 Twits
             </TabPanel>
             <TabPanel value={tabValue} index={1}>
-                Logs
+                <LogTable data={rows}/>
             </TabPanel>
         </Grid>
     </>
